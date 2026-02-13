b12e69c17c8202b481c4132f9761a1af6ff024da
Raj <rajugowda0232@gmail.com>
2026-02-13 08:38:39 +0530

    updated UI, moved to PWA icons to backend for cache support

--- src/main/java/dev/retreever/api/RetreeverUIController.java
+++ src/main/java/dev/retreever/api/RetreeverUIController.java
@@ -1,7 +1,15 @@
 package dev.retreever.api;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class RetreeverUIController {
@@ -15,4 +23,13 @@ public class RetreeverUIController {
     public String forwardReactRoutes() {
         return "forward:/index.html";
     }
+
+    @GetMapping("/assets/icons/{name}")
+    public ResponseEntity<Resource> icon(@PathVariable String name) {
+        Resource r = new ClassPathResource("static/assets/icons/" + name);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).immutable())
+                .body(r);
+    }
+
 }
